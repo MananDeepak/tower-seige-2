1b2f71ef70f97445f46e0df5d99e4fed53e7bea2
MananDeepak <72694194+MananDeepak@users.noreply.github.com>
2020-12-13 11:12:59 +0530

    Add files via upload

---
+++ block.js
@@ -0,0 +1,40 @@
+class Block {
+    constructor(x,y,width,height) {
+      var options = {
+          //isStatic:false,
+       restitution:0.4,
+       friction:0
+      }
+      this.visibility=255;
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      
+     World.add(world, this.body);
+     
+    }
+    display(){
+      if(this.body.speed<3){
+        
+        var angle=this.body.angle;
+        var pos=this.body.position;
+      push();
+      translate(pos.x,pos.y);
+     
+      rectMode(CENTER);
+      rotate(angle)
+      fill("pink");
+      rect(0,0,this.width,this.height);
+      pop();
+    }
+    else{
+      World.remove(world,this.body);
+      push();
+      this.visibility=this.visibility-5;
+     pop();
+
+    }
+      
+    
+    }
+  }
